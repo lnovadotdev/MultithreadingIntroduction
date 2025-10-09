f145dcedb36f3097f7bbcd9c3e2f78906890da01
Luna Nova <luna@lnova.dev>
2025-10-09 16:12:07 +0100

    Complete task 2
    Implemented code to successfully complete task 2

--- GDEV50037 - MGP - T02 - Thread Framework/Task2.cs
+++ GDEV50037 - MGP - T02 - Thread Framework/Task2.cs
@@ -7,6 +7,10 @@ namespace ThreadExample
     {
         public Task2()
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Task2ThreadInfo threadInfo = new Task2ThreadInfo(i);
+            }
         }
     }
 
@@ -16,10 +20,22 @@ namespace ThreadExample
 
         public Task2ThreadInfo(int index)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(new ThreadStart(Counting));
+                t.Name = "Thread " + index;
+                this.index = index;
+                t.Start();
+            }
         }
 
         public void Counting()
         {
+            while (true)
+            {
+                Console.WriteLine(index);
+                Thread.Sleep((int)((index + 1) * 0.5));
+            }
         }
     }
 }
